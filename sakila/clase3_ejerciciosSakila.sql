4e823693359e694fda74c206aae4e964d8983e67
Miguel Flores <142996025+mf2465@users.noreply.github.com>
2024-08-26 23:42:23 -0300

    Update clase3_ejerciciosSakila.sql

--- sakila/clase3_ejerciciosSakila.sql
+++ sakila/clase3_ejerciciosSakila.sql
@@ -19,9 +19,36 @@ select max(amount) as Cantidad_más_alta from sakila.payment;
 -- 5
 select * from sakila.payment order by amount asc;
 -- where
- 
-
-
- 
-
-
+select * from sakila.actor;
+select * from sakila.actor where first_name='ED';
+select * from sakila.city;
+select * from sakila.city where city ='London';
+select * from sakila.city where country_id = 102;
+select * from sakila.inventory where film_id >= 50;
+-- 6
+select * from sakila.payment;
+select distinct amount from sakila.payment where amount < 3 order by amount desc;
+-- 7 
+select * from sakila.staff;
+select * from sakila.staff where staff_id != 2;
+-- 8
+select * from sakila.language;
+select * from sakila.language where name != 'german';
+-- 9
+select * from sakila.film;
+select description, release_year from sakila.film where title = 'IMPACT ALADDIN'; 
+-- 10
+select * from sakila.payment where amount > 0.99 order by amount desc;
+-- 11
+select * from sakila.country where country = 'Algeria' and country_id = 2;
+-- 12
+select * from sakila.country where country = 'Algeria' or country_id = 12;
+-- 13 
+select * from sakila.language where language_id = 1 or name = 'german';
+-- 14
+SELECT * FROM sakila.category;
+select * from sakila.category where name != 'action';
+-- 15
+select * from sakila.film where rating != 'PG';
+-- 16
+select distinct rating from sakila.film where rating != 'PG';
