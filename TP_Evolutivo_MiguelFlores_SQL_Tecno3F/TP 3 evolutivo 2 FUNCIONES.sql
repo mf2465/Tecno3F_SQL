eeea58422e473416aa49b621c9041c28239bf9f3
Miguel Flores <142996025+mf2465@users.noreply.github.com>
2024-11-01 22:49:15 -0300

    Add files via upload

--- TP_Evolutivo_MiguelFlores_SQL_Tecno3F/TP 3 evolutivo 2 FUNCIONES.sql
+++ TP_Evolutivo_MiguelFlores_SQL_Tecno3F/TP 3 evolutivo 2 FUNCIONES.sql
@@ -35,24 +35,36 @@ BEGIN
 END
 $$
 
-SELECT nombre, apellido, get_nombre_empresa(empresa) as 'Empresa', get_cargo_empresa(cargo_empresa) as 'Cargo' FROM usuario order by empresa,cargo_empresa;
+SELECT nombre, apellido, get_nombre_empresa(empresa) as 'Empresa', get_cargo_empresa(cargo_empresa) as 'Cargo',email FROM usuario order by empresa,cargo_empresa;
 
-select* from usuario;
 
-DROP FUNCTION IF EXISTS get_usuario_activo;
+/*
+El departamento comercial lanza una promoción para todas las cotizaciones realizadas en el año 2024
+donde se otorga un beneficio de acuerdo a la forma de pago. Se establece que para la cancelación con transferencia
+amplía el plazo a 90 días, si abona con tarjeta es de 60 y en caso de utilizar cheque, se reduce a 30.
+*/
+
+
+DROP FUNCTION IF EXISTS promo2024;
 
 DELIMITER $$
-CREATE FUNCTION get_usuario_activo (p_last_session date)
-RETURNS date
 
+CREATE FUNCTION promo2024 (p_forma_pago VARCHAR(50))
+RETURNS VARCHAR(255)
 READS SQL DATA
 BEGIN
-    DECLARE respuesta date;
-    SET respuesta = (SELECT last_session FROM usuario WHERE last_session BETWEEN DATE_SUB(NOW(), INTERVAL 180 DAY) AND NOW());
-    RETURN respuesta;
-END
-$$
+    DECLARE beneficio VARCHAR(50);
 
-SELECT nombre, apellido, get_nombre_empresa(empresa) as 'Empresa', get_cargo_empresa(cargo_empresa) as 'Cargo',  
-get_usuario_activo (last_session) as 'last_session'
-FROM usuario order by empresa,cargo_empresa;
+    IF p_forma_pago = 'transferencia' THEN
+        SET beneficio = 'Plazo: 90 días';
+    ELSEIF p_forma_pago = 'tarjeta' THEN
+        SET beneficio = 'Plazo: 60 días';
+    ELSEIF p_forma_pago = 'cheque' THEN
+        SET beneficio = 'Plazo: 30 días';
+    END IF;
+
+    RETURN beneficio;
+END $$
+
+SELECT idPresupuesto, get_nombre_empresa(empresa) AS 'Empresa', fecha, forma_pago, promo2024(forma_pago) AS 'Beneficio_promo2024'
+FROM presupuesto where fecha >= '2024-01-01' order by Beneficio_promo2024 desc ;
