e80dfa6dce30331a83e874207b4dfdaf8c45de3a
Miguel Flores <142996025+mf2465@users.noreply.github.com>
2024-11-05 00:25:58 -0300

    Add files via upload

--- TP_Evolutivo_MiguelFlores_SQL_Tecno3F/TP 3 evolutivo 2 STORE PROCEDURE.sql
+++ TP_Evolutivo_MiguelFlores_SQL_Tecno3F/TP 3 evolutivo 2 STORE PROCEDURE.sql
@@ -18,9 +18,41 @@ CREATE PROCEDURE usuario_inactivo (IN dias_inactividad INT)
 BEGIN
 	DECLARE fecha_limite DATE;
 	SET fecha_limite = DATE_SUB(CURDATE(), INTERVAL dias_inactividad DAY);
-    SELECT 	nombre, apellido, email, last_session FROM usuario WHERE last_session < fecha_limite;
+    SELECT 	nombre, apellido, email, last_session, now() as FECHA_HOY FROM usuario WHERE last_session < fecha_limite AND activacion > 0;
 END
 $$
 
 -- se ingresa la cantidad de días a partir de la cual se considera inactivo
 CALL usuario_inactivo(365);
+
+/*
+Se desea realizar un listado de los certificados próximos a caducar, ya que el personal
+comenzará a tomar vacaciones y es necesario preveer una guardia para atender las necesidades de los clientes
+en este período del año. El procedimiento contempla la cantidad de días a partir del día de la consulta del operador
+*/
+
+SELECT * FROM certificado;
+
+DROP PROCEDURE certificado_proximo_a_vencer;
+
+DELIMITER $$
+
+CREATE PROCEDURE certificado_proximo_a_vencer (IN dias_a_cubrir INT)
+BEGIN
+    SELECT 
+        idCertificado,
+        fecha,
+        ensayo,
+        aptitud,
+        vigencia_dias,
+        precinto,
+        DATE_ADD(fecha, INTERVAL vigencia_dias DAY) AS fecha_vencimiento,
+        now() as FECHA_HOY
+    FROM certificado
+    WHERE DATE_ADD(fecha, INTERVAL vigencia_dias DAY) BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL dias_a_cubrir DAY) AND aptitud > 0;
+
+END
+$$
+
+-- días a preveer de vencimientos de certificados para organizar esquema de licencias del personal
+CALL certificado_proximo_a_vencer (30);
